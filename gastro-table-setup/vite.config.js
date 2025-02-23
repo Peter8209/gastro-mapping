63bda6f8b9d21e62b5c0ce4a8bd8c0c87bdfc9e8
Peter8209 <tutka.peter@gmail.com>
2025-02-23 12:07:06 +0100

    Add files via upload

--- gastro-table-setup/vite.config.js
+++ gastro-table-setup/vite.config.js
@@ -1,15 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="Gastro Mapping - Vite + React App" />
-    <title>Gastro Mapping</title>
-    <link rel="icon" type="image/png" href="./vite.svg" />
-    <base href="./" />
-  </head>
-  <body>
-    <div id="root"></div>
-    <script type="module" src="./src/main.jsx"></script>
-  </body>
-</html>
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: "index.html",
+    },
+  },
+  base: "./",
+});
+
