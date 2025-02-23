7ad418b719b9de6b9edb7aaccb60e0167c64be4b
Peter8209 <tutka.peter@gmail.com>
2025-02-23 13:52:47 +0100

    Add files via upload

--- gastro-table-setup/vite.config.js
+++ gastro-table-setup/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/gastro-mapping/", // Nastavte správne meno repozitára
+  base: "/gastro-mapping/gastro-table-setup/", // Nastavenie správnej cesty
   build: {
     outDir: "dist",
   },
