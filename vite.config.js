5c216c958f661299f1b51ae9535846af5a35f09d
Peter8209 <tutka.peter@gmail.com>
2025-02-23 11:39:01 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  root: "./", // Uistite sa, že root je správne nastavený
+  build: {
+    outDir: "build",
+    rollupOptions: {
+      input: "index.html", // Nastavuje správnu cestu k index.html
+    },
+  },
+  server: {
+    open: true, // Automaticky otvára stránku pri vývoji
+  },
 });
+
